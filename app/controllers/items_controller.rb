daa3a1f67b7bc04a21d923914cc4f300d9f1191c
Masakazu0327 <masakazu.sato.iwt@gmail.com>
2019-05-02 15:32:42 +0900

    登録・更新機能作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,37 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all 
   end
+  def new
+    @item = Item.new
+  end
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to @item
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    #@item = Item.find(params[:id])
+    @item = Item.find(params[:id])
+    #@item.update(item_params)
+    @item.update(item_params)
+    redirect_to @item
+  end
+
+  private
+
+  # params.require(key).permit(filter)
+  # key
+  #   Strong Parameters を適用したい params の key を指定する。
+  # filter
+  #   Strong Parameters で許可するカラムを指定する。
+
+  def item_params
+    params.require(:item).permit(:name, :price, :seller, :description, :email, :image_url)
+  end
+
 end
